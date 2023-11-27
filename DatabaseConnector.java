7234f8b8655789ed27f0f018d6ff2df2aec8f4be
Ankit Yadav <122368673+03yadavankit@users.noreply.github.com>
2023-11-27 22:27:25 +0530

    Create DatabaseConnector.java

---
+++ DatabaseConnector.java
@@ -0,0 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/sudoku";
+    private static final String USER = "your_username";
+    private static final String PASSWORD = "your_password";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void main(String[] args) {
+        try (Connection connection = DatabaseConnector.getConnection()) {
+            if (connection != null) {
+                System.out.println("Connected to the database!");
+            } else {
+                System.out.println("Failed to connect to the database.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
